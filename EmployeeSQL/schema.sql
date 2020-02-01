4c7fbe000eac76aaeb93d9f36644c2276a42e02c
TLatinska <teodoralatinska@hotmail.com>
2020-02-01 00:04:56 -0500

    Final homework submission

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,37 +1,37 @@
 CREATE TABLE employees (emp_no BIGINT PRIMARY KEY,
 					   birth_date DATE,
-					   first_name TEXT,
-					   last_name TEXT,
+					   first_name TEXT NOT NULL,
+					   last_name TEXT NOT NULL,
 					   gender TEXT,
 					   hire_date DATE);
 
 CREATE TABLE departments (dept_no VARCHAR PRIMARY KEY,
-						 dept_name TEXT);
+						 dept_name TEXT NOT NULL);
 						 
 CREATE TABLE dept_emp (emp_no BIGINT,
 					  dept_no VARCHAR,
 					  FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 					  from_date DATE,
-					  to_date DATE,
+					  to_date DATE NOT NULL,
 					  PRIMARY KEY (emp_no, dept_no, from_date));
 
-CREATE TABLE dept_manager (dept_no VARCHAR,
+CREATE TABLE dept_manager (dept_no VARCHAR NOT NULL,
 						  FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 						  emp_no BIGINT,
 						  from_date DATE,
-						  to_date DATE,
+						  to_date DATE NOT NULL,
 						  PRIMARY KEY (emp_no, from_date));
 
 CREATE TABLE titles (emp_no BIGINT,
 					FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-					title TEXT,
+					title TEXT NOT NULL,
 					from_date DATE,
-					to_date DATE,
+					to_date DATE NOT NULL,
 					PRIMARY KEY (emp_no, from_date));
 					
 CREATE TABLE salaries (emp_no BIGINT,
 					  FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-					  salary BIGINT,
+					  salary BIGINT NOT NULL,
 					  from_date DATE,
-					  to_date DATE,
+					  to_date DATE NOT NULL,
 					  PRIMARY KEY (emp_no, from_date));
